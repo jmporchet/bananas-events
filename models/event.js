d41ada22fadb92f4dd40efc44041fc958a86d1ed
= <jm@jmporchet.ch>
2017-08-01 17:50:46 +0200

    refactor: model cannot be instantiated

--- models/event.js
+++ models/event.js
@@ -1,54 +1,45 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../utils/db');
+const Console = console;
 
-class Event {
-  constructor () {
-    /* create table events(
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      info TEXT, date DATETIME,
-      createdAt DATETIME,
-      updatedAt DATETIME);
-    */
-    this.event = sequelize.define('event', {
-      info: {
-        type: Sequelize.STRING
-      },
-      date: {
-        type: Sequelize.DATE
-      }
-    });
+const Event = sequelize.define('event', {
+  info: {
+    type: Sequelize.STRING
+  },
+  date: {
+    type: Sequelize.DATE
   }
+});
 
-  async getEvents () {
-    try {
-      return await this.event.findAll();
-    } catch (error) {
-      console.error(error);
-    }
+module.exports.getEvents = async () => {
+  try {
+    return await this.event.findAll();
+  } catch (error) {
+    Console.error(error);
   }
+};
 
-  async createEvent (info) {
-    const date = new Date();
-    try {
-      return await this.event.create({info: info.join(), date});
-    } catch (error) {
-      console.error(error);
-    }
+module.exports.getNextEvent = async () => {
+  try {
+    return await this.event.findOne({ order: [ ['date', 'ASC']]});
+  } catch (error) {
+    Console.error(error);
   }
+};
 
-  async deleteEvent (id) {
-    try {
-      console.log('deleting',id);
-      return await this.event.destroy({
-        where: {
-          id: id
-        }
-      });
-    } catch (error) {
-      console.error(error);
-    }
+module.exports.createEvent = async (info) => {
+  const date = new Date();
+  try {
+    return await this.event.create({info: info, date});
+  } catch (error) {
+    Console.error(error);
   }
+};
 
-}
-
-module.exports = Event;
+module.exports.deleteEvent = async (id) => {
+  try {
+    return await this.event.destroy({ where: { id: id } });
+  } catch (error) {
+    Console.error(error);
+  }
+};
