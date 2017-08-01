52fa22a0e2fcd3a8f3d46b7d3c2cea8519771b6f
= <jm@jmporchet.ch>
2017-08-01 17:47:18 +0200

    refactor: model cannot be instantiated

--- models/event.js
+++ models/event.js
@@ -2,57 +2,44 @@ const Sequelize = require('sequelize');
 const sequelize = require('../utils/db');
 const Console = console;
 
-class Event {
-  constructor () {
-    /* create table events(
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      info TEXT, date DATETIME,
-      createdAt DATETIME,
-      updatedAt DATETIME);
-    */
-    this.event = sequelize.define('event', {
-      info: {
-        type: Sequelize.STRING
-      },
-      date: {
-        type: Sequelize.DATE
-      }
-    });
+const Event = sequelize.define('event', {
+  info: {
+    type: Sequelize.STRING
+  },
+  date: {
+    type: Sequelize.DATE
   }
+});
 
-  async getEvents () {
-    try {
-      return await this.event.findAll();
-    } catch (error) {
-      Console.error(error);
-    }
+module.exports.getEvents = async () => {
+  try {
+    return await this.event.findAll();
+  } catch (error) {
+    Console.error(error);
   }
+};
 
-  async getNextEvent () {
-    try {
-      return await this.event.findOne({ order: [ ['date', 'ASC']]});
-    } catch (error) {
-      Console.error(error);
-    }
+module.exports.getNextEvent = async () => {
+  try {
+    return await this.event.findOne({ order: [ ['date', 'ASC']]});
+  } catch (error) {
+    Console.error(error);
   }
+};
 
-  async createEvent (info) {
-    const date = new Date();
-    try {
-      return await this.event.create({info: info, date});
-    } catch (error) {
-      Console.error(error);
-    }
+module.exports.createEvent = async (info) => {
+  const date = new Date();
+  try {
+    return await this.event.create({info: info, date});
+  } catch (error) {
+    Console.error(error);
   }
+};
 
-  async deleteEvent (id) {
-    try {
-      return await this.event.destroy({ where: { id: id } });
-    } catch (error) {
-      Console.error(error);
-    }
+module.exports.deleteEvent = async (id) => {
+  try {
+    return await this.event.destroy({ where: { id: id } });
+  } catch (error) {
+    Console.error(error);
   }
-
-}
-
-module.exports = Event;
+};
