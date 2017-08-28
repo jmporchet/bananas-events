dda15abbbcba2faff04b02eb9ab41713590365a4
= <jm@jmporchet.ch>
2017-08-28 19:02:04 +0200

    refactor: remove references to padel

--- utils/strings.js
+++ utils/strings.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const strings = {
-  HELP: 'Try /padel create <your event>, /padel list, or /padel delete <id>',
-  INVALID_COMMAND: 'This is not a valid command. Try /padel next or /padel create <event info>',
-  NO_EVENTS: 'There are no events. You can create one by typing /padel create <your event>',
+  HELP: 'Try /event create <your event>, /event list, or /event delete <id>',
+  INVALID_COMMAND: 'This is not a valid command. Try /event next or /event create <event info>',
+  NO_EVENTS: 'There are no events. You can create one by typing /event create <your event>',
   EVENT_LIST: 'Here\'s the list of the next events:'
 };
 
